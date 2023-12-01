7a52f6bac1724a0f1077c2ea407597455d8eb850
prithvirajpatil29 <prithvirajkpatil29@gmail.com>
2023-12-01 09:05:02 +0530

    class10

--- class10-music-player/src/App.js
+++ class10-music-player/src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Menu from './components/Menu';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Music from './components/Music';
+import Contact from './components/Contact';
+import Track  from './components/Track';
+import Pnf from './components/Pnf';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Menu></Menu>
+      <Routes>
+        <Route path={'/'} element={<Music></Music>}></Route>
+        <Route path={'/contact'} element={<Contact/>}></Route>
+        <Route path={'/track/:artistId'} element={<Track/>}></Route>
+        <Route path={'/*'} element={<Pnf></Pnf>}></Route>
+      </Routes>
+    </BrowserRouter>
+    );
 }
-
 export default App;
