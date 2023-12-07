2c25eda91aa0ec07ba1f3805244fb0cac4708bd9
prithvirajpatil29 <prithvirajkpatil29@gmail.com>
2023-12-07 12:02:23 +0530

    class11

--- class11-crud/src/App.js
+++ class11-crud/src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { ToastContainer, toast } from 'react-toastify';
+import Menu from './component/Menu';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './component/Login'
+import Register from './component/Register';
+import Pnf from './component/Pnf';
+import Home from './component/Home';
+import ProtectedRoute from './authGaurd/ProtectedRoute';
+import Create from './component/Create';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Menu></Menu>
+      <ToastContainer autoClose={4000} position={'top-center'}></ToastContainer>
+      <Routes >
+        <Route element={<ProtectedRoute></ProtectedRoute>}>
+          <Route path={'/'} element={<Home></Home>}></Route>
+          <Route path={'/create'} element={<Create></Create>}></Route>
+        </Route>
+        <Route path={'/'} element={<Home></Home>}></Route>
+        <Route path={'/login'} element={<Login></Login>}></Route>
+        <Route path={'/register'} element={<Register></Register>}></Route>
+        <Route path={'/*'} element={<Pnf></Pnf>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
