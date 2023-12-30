f2e9e0aa56eedb3a236111c91e0a7efd797e5af0
prithvirajpatil29 <prithvirajkpatil29@gmail.com>
2023-12-30 18:16:18 +0530

    class12

--- class12-server-validation/src/index.js
+++ class12-server-validation/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import 'react-toastify/dist/ReactToastify.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
