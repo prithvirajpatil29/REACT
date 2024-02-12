2416f6905daa22e899b4636dff33400838b66be8
prithvirajpatil29 <prithvirajkpatil29@gmail.com>
2024-02-12 10:04:58 +0530

    context-app

---
+++ class14-contextapp/src/components/Pnf.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Pnf = () => {
+  return (
+    <div className="container">
+        <div className="row">
+            <div className="col text-center">
+                <h3 className="display-3 text-danger">Page Not Found</h3>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Pnf
