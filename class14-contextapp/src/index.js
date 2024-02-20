28e3c0425744e1d62002f2ca462fd4ee7ea0830a
prithvirajpatil29 <prithvirajkpatil29@gmail.com>
2024-02-20 17:06:58 +0530

    class14

--- class14-contextapp/src/index.js
+++ class14-contextapp/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import 'react-toastify/dist/ReactToastify.css';
+import DataProvider from './context/Context'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <DataProvider>
     <App />
+    </DataProvider>
   </React.StrictMode>
 );
 
